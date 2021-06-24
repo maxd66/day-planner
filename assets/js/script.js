e75da95af049d299607c38f3ebe28d0d5757d0c9
Maxwell Dunn <max.dunn63@gmail.com>
2021-06-23 21:24:37 -0700

    added time responsiveness to text area and fixed button layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// need timer to constantly update the page with new time and day
 // timer will dictate color of text areas throughout the day 
 //      for loop, loop through ids in an array and set if statements
 //      if current time matches id, starting at nine would be i+9
@@ -8,4 +7,29 @@
 setInterval(function(){
     var currentDate = moment().format('MMMM Do, YYYY   hh:mm:ss');
     document.querySelector('#currentDay').textContent = currentDate;
+    updateTimeSlots();
 }, 1000)
+
+var timeArr = ['hour-9', 
+    'hour-10', 
+    'hour-11', 
+    'hour-12', 
+    'hour-13', 
+    'hour-14', 
+    'hour-15', 
+    'hour-16', 
+    'hour-17'];
+
+var currentTime = moment().format('H')
+
+function updateTimeSlots() {
+    for(var i = 0; i <timeArr.length; i++) {
+        if((i + 9) == currentTime) {
+            document.getElementById(timeArr[i]).setAttribute('class', 'mediaRow row time-block present')
+        } else if((i + 9) < currentTime) {
+            document.getElementById(timeArr[i]).setAttribute('class', 'mediaRow row time-block past')
+        } else {
+            document.getElementById(timeArr[i]).setAttribute('class', 'mediaRow row time-block future')
+        }
+    }
+};
